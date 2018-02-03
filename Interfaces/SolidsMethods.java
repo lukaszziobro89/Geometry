9911ab20308f9043b0b67bbbee4df4f267b8b4f8
Lukasz Ziobro <lukasz.ziobro.89@gmail.com>
2018-02-03 22:59:32 +0100

    Solid class interface

--- Interfaces/SolidsMethods.java
+++ Interfaces/SolidsMethods.java
@@ -1,4 +1,12 @@
 package Interfaces;
 
 public interface SolidsMethods {
+    /** Calculates base area of solid */
+    double calculateBaseArea();
+
+    /** Calculate side area of solid */
+    double calculateSideArea();
+
+    /** Calculate volume of solid */
+    double calculateVolume();
 }
