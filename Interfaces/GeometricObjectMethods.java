48f998752285a44b46a94af4f7b9f53177182f44
Lukasz Ziobro <lukasz.ziobro.89@gmail.com>
2018-02-03 22:58:38 +0100

    GeometricObject interface

---
+++ Interfaces/GeometricObjectMethods.java
@@ -0,0 +1,11 @@
+package Interfaces;
+
+public interface GeometricObjectMethods {
+    /** Prints information about object creation date */
+    void printCreationDate();
+
+    /** Prints information about object */
+    void printObjectInformation();
+
+
+}
